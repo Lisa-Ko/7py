7a1399a9d895e67d9988487bd0e59d8ee6dd0a2e
drkop <satanatyn@yandex.ru>
2023-06-02 19:31:04 +0300

    ex

---
+++ 1.py
@@ -0,0 +1,15 @@
+def rhythm(str):
+    str = str.split()
+    list_1 = []
+    for word in str:
+        sum_w = 0
+        for i in word:
+            if i in 'аеёиоуыэюя':
+                sum_w += 1
+        list_1.append(sum_w)
+    return len(list_1) == list_1.count(list_1[0])
+str_1 = input("Веведите стихотворение: ")
+if rhythm(str_1):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
